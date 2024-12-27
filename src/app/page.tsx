5bb48c5015626cd06eded10a0fb056bde8716f41
mohan <niyalailea@gmail.com>
2024-12-27 17:24:18 +0900

    chore: Vite から Next.js へ移行

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+// app/page.tsx
+import React from 'react';
+
+const Page = () => {
+  return (
+    <main>
+      <h1>Welcome to Next.js</h1>
+      <p>NRTZ start！！！</p>
+    </main>
+  );
+};
+
+export default Page;
