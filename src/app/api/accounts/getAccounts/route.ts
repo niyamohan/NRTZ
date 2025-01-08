567bf9d6a4439d4d6093a2a4cfa1ac5af685256d
mohan <niyalailea@gmail.com>
2025-01-08 17:42:25 +0900

    fix: 不要のAPIフォルダーおよびファイルの削除

--- src/app/api/accounts/getAccounts/route.ts
+++
@@ -1,6 +0,0 @@
-import  { fakeDb } from  '../../../../lib/db'; // データベース接続モジュール
-
-export async function GET() {
-  const accounts = fakeDb.getAccounts();
-  return new Response(JSON.stringify(accounts));
-}
--- src/app/api/patients/getPatients/route.ts
+++
@@ -1,6 +0,0 @@
-import { fakeDb } from '@/lib/db';
-
-export async function GET() {
-  const patients =  fakeDb.getPatients();
-  return new Response(JSON.stringify(patients));
-} 
